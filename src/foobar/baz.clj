58ffba20ab25743ce2c100e0c4670c3a4fb7d26d
lread <lee@dlread.com>
2024-11-17 15:46:07 -0500

    Review readme/updates

--- src/foobar/baz.clj
+++ src/foobar/baz.clj
@@ -8,18 +8,15 @@
 ;; our clj-kondo hook adds a finding when `something` is `foo`
 
 ;; no problem, `something` is not `foo`
-(dingo bar
-  (str bar))
+(dingo bar (str bar))
 ;; => "42"
 
 ;; problem, `something` is `foo`, clj-kondo should not report the problem because we've
 ;; ignored it
 #_{:clj-kondo/ignore [:acme/foo-not-good]}
-(dingo foo
-  (str foo))
+(dingo foo (str foo))
 ;; => "42"
 
 ;; problem, `something` is `foo`, clj-kondo should report the problem
-(dingo foo
-  (str foo))
+(dingo foo (str foo))
 ;; => "42"
